e1703fac81c1f69a94a59607d79ec16a2b26da7f
Vincent Marcus <boyplankton@gmail.com>
2023-05-13 15:57:54 -0600

    reducing the number of findings in the linter

--- js/dig.wtf.js
+++ js/dig.wtf.js
@@ -1,5 +1,60 @@
 window.APP = window.APP || {}
 
+function parseAnswers (answers, dnsType) {
+  var response = {
+    name: '',
+    ttl: 0,
+    type: '',
+    value: [],
+    desc: ''
+  }
+
+  answers.forEach(answer => {
+    response.name = answer.name
+    response.ttl = answer.ttl
+    response.type = answer.type
+    response.value.push(answer.data)
+    response.desc = dnsType.desc
+  })
+
+  return response
+}
+
+function formatA (data) {
+  return '<a href="https://talosintelligence.com/reputation_center/lookup?search=' + data + '">' + data + '</a>'
+}
+
+function formatCNAME (data) {
+  return '<a href="#/dig/' + data + '">' + data + '</a>'
+}
+
+function formatMX (data) {
+  return data.preference + ' ' + data.exchange
+}
+
+function formatSOA (data) {
+  return data.mname + '. ' + data.rname + '. ' + data.serial + ' ' + data.refresh + ' ' + data.retry + ' ' + data.expire + ' ' + data.minimum
+}
+
+function formatAnswer (type, answer) {
+  switch (type) {
+    case 'A':
+      answer = formatA(answer)
+      break
+    case 'CNAME':
+      answer = formatCNAME(answer)
+      break
+    case 'MX':
+      answer = formatMX(answer)
+      break
+    case 'SOA':
+      answer = formatSOA(answer)
+      break
+  }
+
+  return answer
+}
+
 APP.DigModel = Backbone.Model.extend({
 })
 
@@ -8,7 +63,7 @@ APP.DigCollection = Backbone.Collection.extend({
 })
 
 APP.DigSearchView = Backbone.View.extend({
-  el: $('#searchForm'),
+  el: $('#search-form'),
 
   events: {
     // listen for the submit event of the form
@@ -29,7 +84,7 @@ APP.DigSearchView = Backbone.View.extend({
 APP.DigResultsView = Backbone.View.extend({
   el: $('#results'),
 
-  template: _.template($('#digTemplate').html()),
+  template: _.template($('#dig-template').html()),
 
   render: function () {
     var results = { results: [] }
@@ -56,99 +111,8 @@ APP.DigRouter = Backbone.Router.extend({
   },
 
   dig: function (host) {
-    var self = this
     var timeout = 1000
     var url = 'https://cloudflare-dns.com/dns-query'
-
-    /*
-    const type_map = {
-      1: "A",
-      2: "NS",
-      3: "MD",
-      4: "MF",
-      5: "CNAME",
-      7: "MB",
-      8: "MG",
-      9: "MR",
-      11: "WKS",
-      12: "PTR",
-      13: "HINFO",
-      14: "MINFO",
-      15: "MX",
-      16: "TXT",
-      17: "RP",
-      18: "AFDB",
-      19: "X25",
-      20: "ISDN",
-      21: "RT",
-      22: "NSAP",
-      23: "NSAP-PTR",
-      24: "SIG",
-      25: "KEY",
-      26: "PX",
-      27: "GPOS",
-      28: "AAAA",
-      29: "LOC",
-      30: "NXT",
-      31: "EID",
-      32: "NIMLOC",
-      33: "SRV",
-      34: "ATMA",
-      35: "NAPTR",
-      36: "KX",
-      37: "CERT",
-      38: "A6",
-      39: "DNAME",
-      40: "SINK",
-      41: "OPT",
-      42: "APL",
-      43: "DS",
-      44: "SSHFP",
-      45: "IPSECKEY",
-      46: "RRSIG",
-      47: "NSEC",
-      48: "DNSKEY",
-      49: "DHCID",
-      50: "NSEC3",
-      51: "NSEC3PARAM",
-      52: "TLSA",
-      53: "SMIMEA",
-      55: "HIP",
-      56: "NINFO",
-      57: "RKEY",
-      58: "TALINK",
-      59: "CDS",
-      60: "CDNSKEY",
-      61: "OPENPGPKEY",
-      62: "CSYNC",
-      63: "ZONEMD",
-      64: "SVCB",
-      65: "HTTPS",
-      99: "SPF",
-      100: "UINFO",
-      101: "UID",
-      102: "GID",
-      103: "UNSPEC",
-      104: "NID",
-      105: "L32",
-      106: "L64",
-      107: "LP",
-      108: "EUI48",
-      109: "EUI64",
-      249: "TKEY",
-      250: "TSIG",
-      251: "IXFR",
-      252: "AXFR",
-      253: "MAILB",
-      254: "MAILA",
-      256: "URI",
-      257: "CAA",
-      258: "AVC",
-      259: "DOA",
-      260: "AMTRELAY",
-      32768: "TA"
-    };
-    */
 
     const dnsTypes = [
       { type: 'A', desc: 'IPv4 Address' },
